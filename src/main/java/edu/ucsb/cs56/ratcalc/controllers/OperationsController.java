2e32cf418697970b3ad9346a7c838b53567e3fac
Peizhen Tong <ptong@ucsb.edu>
2020-02-02 16:57:19 -0800

    pt - added controller method to show divide form

--- src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
+++ src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
@@ -145,14 +145,14 @@ public class OperationsController {
         ratCalcForm.setOp("/");
 
         // TODO: Fill this in with appropriate code
-	if (!bindingResult.hasErrors() && !checkDenominatorErrors(ratCalcForm)&&!checkDivideByZero(ratCalcForm)) {
+	if (!checkDivideByZero(ratCalcForm)&&!bindingResult.hasErrors() && !checkDenominatorErrors(ratCalcForm)) {
             Rational r1 = new Rational(ratCalcForm.getNum1(), ratCalcForm.getDenom1());
             Rational r2 = new Rational(ratCalcForm.getNum2(), ratCalcForm.getDenom2());
 	    
-		Rational result = Rational.quotient(r1, r2);
+         	Rational result = Rational.quotient(r1, r2);
 		logger.info("r1=" + r1 + " r2=" + r2 + " result=" + result);
 		ratCalcForm.setNumResult(result.getNumerator());
-		ratCalcForm.setDenomResult(result.getDenominator());
+                ratCalcForm.setDenomResult(result.getDenominator());
 	    
         }
         
